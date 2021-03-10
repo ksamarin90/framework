ba747433bab7dfb9278a27559d301b1e04587e4d
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2021-03-11 00:30:23 +0300

    Created Attributes and Sync classes, refactor User class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Hooo', age: 555 })
 
+user.events.on('change', () => {
+    console.log('object')
+})
+
+user.events.trigger('change')
 // user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,38 +1,18 @@
-import axios, { AxiosResponse } from "axios";
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
-    constructor(private data: UserProps) { }
+    public events: Eventing = new Eventing();
+    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
 
-    get(propName: string): (string | number) {
-        return this.data[propName];
-    }
-
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-            .then((res: AxiosResponse): void => {
-                this.set(res.data);
-            })
-    }
-
-    save(): void {
-        const id = this.get('id')
-        const url = `http://localhost:3000/users`
-        if (id) {
-            axios.put(`${url}/${id}`, this.data);
-        } else {
-            axios.post(url, this.data)
-        }
-    }
 
 }
 
