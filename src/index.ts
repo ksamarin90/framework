9b3f247e9edcb15470cffcabd7159db7938a9173
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2021-03-12 00:06:38 +0300

    refactored Attributes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import { User } from './models/User'
 
 const user = new User({ name: 'Hooo', age: 555 })
-
-user.events.on('change', () => {
-    console.log('object')
-})
-
-user.events.trigger('change')
-// user.save();
