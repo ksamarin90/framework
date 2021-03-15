864903e1b09211e9d685bf7c0b5f0ab59579364b
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2021-03-15 14:34:19 +0300

    re-render model + strict mode enabled

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
+const root = document.getElementById('root');
 
-const userForm = new UserForm(document.getElementById('root'), User.buildUser({ name: 'Steve', age: 20 }))
-
-userForm.render()
+if (root) {
+    const userForm = new UserForm(root, User.buildUser({ name: 'Steve', age: 20 }));
+    userForm.render();
+} else {
+    throw new Error('Root element not found');
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,15 +1,31 @@
 import { User } from '../models/User';
 
 export class UserForm {
-    constructor(public parent: Element, public model: User) {}
+    constructor(public parent: Element, public model: User) {
+        this.bindModel();
+    }
+
+    bindModel(): void {
+        this.model.on('change', () => {
+            this.render();
+        });
+    }
 
     onSetAgeClick = (): void => {
         this.model.setRandomAge();
     }
 
+    onSetNameClick = (): void => {
+        const input = this.parent.querySelector('input');
+        if (input) {
+            this.model.set({ name: input.value});
+        }
+    }
+
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-age': this.onSetAgeClick,
+            'click:.set-name': this.onSetNameClick,
         };
     }
 
@@ -19,7 +35,8 @@ export class UserForm {
                 <h1>User Form</h1>
                 <div>User name: ${this.model.get('name')}</div>
                 <div>User age: ${this.model.get('age')}</div>
-                <button>Click</button>
+                <input />
+                <button class="set-name">Change Name</button>
                 <button class="set-age">Set Random Age</button>
             </div>
         `;
@@ -37,6 +54,7 @@ export class UserForm {
     }
 
     render(): void {
+        this.parent.innerHTML = '';
         const templateElement = document.createElement('template');
         templateElement.innerHTML = this.template();
 
