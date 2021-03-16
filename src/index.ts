7164d265fb2feca6c21a2d8fdf446af114ff6e99
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2021-03-16 14:48:54 +0300

    Created UserEdit and UserShow classes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const root = document.getElementById('root');
 
 if (root) {
-    const userForm = new UserForm(root, User.buildUser({ name: 'Steve', age: 20 }));
-    userForm.render();
+    const userEdit = new UserEdit(root, User.buildUser({ name: 'Steve', age: 20 }));
+    userEdit.render();
+    console.log(userEdit)
 } else {
     throw new Error('Root element not found');
 }
